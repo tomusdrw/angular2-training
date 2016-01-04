c306045fbe4eaa9ef1c677d3a23c4f96ccf609e2
Tomusdrw <tomusdrw@gmail.com>
2016-01-04 18:06:19 +0100

    First presentation

---
+++ intro/app.ts
@@ -0,0 +1,12 @@
+import {Component} from 'angular2/core';
+import {bootstrap} from 'angular2/bootstrap';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>{{ hello }}</h1>'
+})
+class AppComponent { 
+    public hello:string = 'Hello, world!';
+}
+
+bootstrap(AppComponent);
